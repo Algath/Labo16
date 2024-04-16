be2fc5f1cceed9f6aa5ab25516909ecc4d5cc3d2
Discretos <siedeltoad@gmail.com>
2024-04-16 15:37:04 +0200

    Tache_1™

---
+++ src/ArrayFactory.scala
@@ -0,0 +1,89 @@
+import scala.util.Random
+
+trait ArrayFactory {
+
+  def create(size: Int): Array[Int]
+
+}
+
+object RandomArrayFactory extends ArrayFactory {
+  override def create(size: Int): Array[Int] = {
+
+    var t:Array[Int] = Array.ofDim(size)
+
+    for(i:Int <- 0 until size){
+      t(i) = Random.nextInt(size + 1)
+    }
+
+    return t
+
+  }
+
+}
+
+object InvertedSortedArrayFactory extends ArrayFactory {
+  override def create(size: Int): Array[Int] = {
+
+    var t: Array[Int] = Array.ofDim(size)
+
+    for (i: Int <- 1 until size + 1) {
+      t(i) = size - i
+    }
+
+    return t
+
+  }
+
+}
+
+object ShuffleArrayFactory extends ArrayFactory {
+  override def create(size: Int): Array[Int] = {
+
+    var t: Array[Int] = Array.ofDim(size)
+
+    var p:Int = 0
+    for (i: Int <- 0 until size) {
+
+      if(i % 2 == 0) {
+        t(i) = p
+        p += 1
+      }
+      else{
+        t(i) = size - p
+      }
+
+    }
+
+    return t
+
+  }
+
+}
+
+object AlmostSortedArrayFactory extends ArrayFactory {
+
+  var percent:Int = 100
+
+  override def create(size: Int): Array[Int] = {
+
+    var t:Array[Int] = Array.ofDim(size)
+
+    for (i: Int <- 0 until size) {
+      t(i) = i
+    }
+
+    for (i: Int <- 0 until percent / 2) {
+
+      var r:Int = Random.nextInt(size / 2)
+
+      var x:Int = t(r)
+      t(r) = t(size - r)
+      t(size - r) = x
+
+    }
+
+  return t
+
+  }
+
+}
