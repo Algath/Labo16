eea46a8287f89c1fe70b1c0027884d6c884b7c34
Algath <119356079+Algath@users.noreply.github.com>
2024-04-16 15:47:54 +0200

    possibilité de tester dedans fait

--- src/SortApplication.scala
+++ src/SortApplication.scala
@@ -1,7 +1,12 @@
-class SortApplication {
-  def displayArray(n: Array[Int]): Unit = {
-    for (i <- n.indices) {
-      println(n(i))
+object SortApplication extends App {
+  class SortApplication {
+    def displayArray(n: Array[Int]): Unit = {
+      for (i <- n.indices) {
+        println(n(i))
+      }
     }
   }
+
+  var test: SortApplication = new SortApplication
+  test.displayArray(RandomArrayFactory.create(10))
 }
